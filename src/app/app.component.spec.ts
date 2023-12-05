18d0c13324d110cef5a2cdce2155812dce2bae16
Jesús Guijarro <72225659+JesusEduardoGuijarroSaldana@users.noreply.github.com>
2023-12-05 14:06:12 -0600

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CBDF_JEGS_12');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, CBDF_JEGS_12'
+    );
   });
 });
---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculadora',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css',
+})
+export class CalculadoraComponent {
+  numero1: number = 2;
+  numero2: number = 5;
+  resultado: number = 7;
+
+  sumarNumeros() {
+    this.resultado = this.numero1 + this.numero2;
+  }
+  restarNumeros() {
+    this.resultado = this.numero1 - this.numero2;
+  }
+  multiplicarNumeros() {
+    this.resultado = this.numero1 * this.numero2;
+  }
+  dividirNumeros() {
+    if (this.numero2 == 0) alert('No se puede realizar la división.');
+    else this.resultado = this.numero1 / this.numero2;
+  }
+}
